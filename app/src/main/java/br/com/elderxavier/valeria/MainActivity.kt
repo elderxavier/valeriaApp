e4332a7cdfedd76f091c96caf9bd94729fe67da8
Elder Xavier <eldersxavier@gmail.com>
2018-03-26 12:16:16 -0300

    comentado menu setings

--- app/src/main/java/br/com/elderxavier/valeria/MainActivity.kt
+++ app/src/main/java/br/com/elderxavier/valeria/MainActivity.kt
@@ -39,11 +39,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }
+    }*/
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
